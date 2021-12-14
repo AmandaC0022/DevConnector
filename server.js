1bdcdb954208ffdd7b5909df68a7125392fcd2cb
Amanda Morgan <AmandaC0022@gmail.com>
2021-12-14 12:56:20 -0600

    installed dependencies

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express'); 
+
+const app = express(); 
+
+const PORT = process.env.PORT || 4000; 
+
+app.get('/', (req, res) => (res.send("API running!"))); 
+
+app.listen(PORT, () => {
+    console.log(`server started on ${PORT}.`); 
+}); 
