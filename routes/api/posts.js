b23a3c2cc991bcb0e2f3d80dab78d7cf8b11106e
Amanda Morgan <AmandaC0022@gmail.com>
2021-12-17 10:41:11 -0600

    can now unlike a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -103,11 +103,34 @@ router.put('/like/:id', auth, async (req, res) => {
 
         //Check if the post has already been liked by user 
         if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
-            return res.json(404).json({ msg: "Post has already been liked."}); 
+            return res.status(404).json({ msg: "Post has already been liked."}); 
         }; 
 
         //add user to the likes 
         post.likes.unshift({ user: req.user.id }); 
+        //save it 
+        await post.save(); 
+        res.json(post.likes); 
+    } catch (err) {
+        console.error(err.message); 
+        res.status(500).send("Server Error"); 
+    }
+}); 
+
+//Unlike a Post
+router.put('/unlike/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id); 
+
+        //Check if the post has already been liked by user 
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+            return res.status(404).json({ msg: "Post has not been liked."}); 
+        }; 
+
+        //get remove index 
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id); 
+        post.likes.splice(removeIndex, 1); 
+         
         //save it 
         await post.save(); 
         res.json(post.likes); 
