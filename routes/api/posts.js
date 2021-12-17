e5410dc1a52f6dbd85d3cd90b6aa29d7e74b75ec
Amanda Morgan <AmandaC0022@gmail.com>
2021-12-17 10:01:39 -0600

    able to get all posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -36,4 +36,16 @@ router.post('/', [auth, [
     } 
 }); 
 
+//Get all posts 
+router.get('/', auth, async (req, res) => {
+    try {
+        //this sorts by the newest date
+        const posts = await Post.find().sort({ date: -1 }); 
+        res.json(posts); 
+    } catch (err) {
+        console.error(err.message); 
+        res.status(500).send("Server Error"); 
+    }
+}); 
+
 module.exports = router; 
