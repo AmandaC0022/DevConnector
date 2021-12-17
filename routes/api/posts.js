35f6d4ff21076260c12b5f33d1f6dc196d78c0d5
Amanda Morgan <AmandaC0022@gmail.com>
2021-12-17 09:55:04 -0600

    user is able to add a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,39 @@
 const express = require('express'); 
 const router = express.Router(); 
+const { check, validationResult } = require('express-validator'); 
+const auth = require('../../middleware/auth'); 
+const User = require('../../models/User'); 
+const Profile = require('../../models/Profile'); 
+const Post = require('../../models/Post'); 
 
-//test GET route 
-router.get('/', (req, res) => {
-    res.send('Post route'); 
+//create new post 
+router.post('/', [auth, [
+    check('text', 'Text is required.').not().isEmpty(),
+]], async (req, res) => {
+
+    const errors = validationResult(req); 
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() }); 
+    }
+
+    try {
+        //find user by id and exclude the password 
+        const user = await User.findById(req.user.id).select('-password'); 
+
+        const newPost = new Post ({
+            text: req.body.text, 
+            name: user.name, 
+            avatar: user.avatar, 
+            user: req.user.id, 
+        }); 
+
+        const post = await newPost.save(); 
+        res.json(post); 
+
+    } catch (err) {
+        console.error(err.message); 
+        res.status(500).send("Server Error"); 
+    } 
 }); 
 
 module.exports = router; 
