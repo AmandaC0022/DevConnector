5e3cf5db40701710f899e14a6ec12714ea21eb80
Amanda Morgan <AmandaC0022@gmail.com>
2021-12-26 09:23:36 -0600

    added signup and login routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React, { Fragment } from 'react'; 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import './App.css';
+import Login from './components/auth/Login';
 import Landing from './components/layout/Landing';
 import Navbar from "./components/layout/Navbar";
+import Register from './components/auth/Register';
 
 const App = () => {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Landing />} /> 
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />  
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
